30247df26f5ac35580b89373b5d9b5cc305748c4
German Cabrera <c.germanjesus@gmail.com>
2023-01-16 17:02:48 -0300

    preentrega 2

---
+++ Producto.js
@@ -0,0 +1,9 @@
+export class Producto {
+    constructor(id, nombre, precio, tipo, marca) {
+      this.id = id;
+      this.nombre = nombre;
+      this.precio = precio;
+      this.tipo = tipo;
+      this.marca = marca;
+    }
+  }
--- index.js
+++ index.js
@@ -1,51 +1,87 @@
+import {Producto} from "./Producto.js";
+let prod1= new Producto (1, 'B450 gaming plus', 5500, 'Mother', 'Asus');
+let prod2= new Producto (2, 'Ryzen 5', 7000, 'Procesador', 'AMD');
+let prod3= new Producto (3, 'Orion rgb 16gb', 5000, 'Memoria', 'Geil');
+let prod4= new Producto (4, 'Thor 850w', 4500, 'Fuente', 'Asus');
+
+
 function compra() {
   alert("Buenos dias, bienvenido a Hardware World")
-  alert("ingrese la opcion correspondiente a su producto")
-  let productos = Number(prompt("1-mother $5500 \n 2-procesador $7000 \n 3-memoria $5000 \n 4-fuente $4500 \n 0-salir"))
+  alert("Ingrese la opcion correspondiente a su producto")
+  let productos = Number(prompt(" 1 - "+prod1.tipo+" "+prod1.nombre+" $"+prod1.precio+"\n 2 - "+prod2.tipo+" "+prod2.nombre+" $"+prod2.precio+"\n 3 - "+prod3.tipo+" "+prod3.nombre+" $"+prod3.precio+"\n 4 - "+prod1.tipo+" "+prod4.nombre+" $"+prod4.precio+ "\n -1 - Eliminar producto \n 0 - Salir"))
   let cantidadProducto;
+  let productoEliminar;
   let total = 0;
+  let indice= 0;
+  let carrito = []
 
   while (productos != 0) {
     switch (productos) {
       case 1:
-        cantidadProducto = Number(prompt("usted ingreso mother, indique la cantidad de componentes"))
-        total += cantidad(cantidadProducto, 5500)
+        cantidadProducto = Number(prompt("Usted ingreso "+prod1.tipo+" "+prod1.nombre+", indique la cantidad de componentes"))
+          for (let i = 0; i < cantidadProducto; i++) {
+            carrito.push(prod1)
+            console.log(i,carrito)
+          }
         break;
       case 2:
-        cantidadProducto = Number(prompt("usted ingreso procesador, indique la cantidad de componentes"))
-        total += cantidad(cantidadProducto, 7000)
+        cantidadProducto = Number(prompt("Usted ingreso "+prod2.tipo+" "+prod2.nombre+", indique la cantidad de componentes"))
+        for (let i = 0; i < cantidadProducto; i++) {
+          carrito.push(prod2)
+        }
         break;
       case 3:
-        cantidadProducto = Number(prompt("usted ingreso memoria, indique la cantidad de componentes"))
-        total += cantidad(cantidadProducto, 5000)
+        cantidadProducto = Number(prompt("Usted ingreso "+prod3.tipo+" "+prod3.nombre+", indique la cantidad de componentes"))
+        for (let i = 0; i < cantidadProducto; i++) {
+          carrito.push(prod3)
+        }
         break;
       case 4:
-        cantidadProducto = Number(prompt("usted ingreso fuente, indique la cantidad de componentes"))
-        total += cantidad(cantidadProducto, 4500)
+        cantidadProducto = Number(prompt("Usted ingreso "+prod4.tipo+" "+prod4.nombre+", indique la cantidad de componentes"))
+        for (let i = 0; i < cantidadProducto; i++) {
+          carrito.push(prod4)
+        }
         break;
+      case -1:
+        let p="";
+        alert("Seleccione el producto que desea eliminar")
+        carrito.forEach(producto=> {
+          p= p + "\n" + indice + "- " + producto.nombre 
+          indice++;
+        })
+        productoEliminar = Number(prompt(p))
+        carrito.splice(productoEliminar,1)
+        indice= 0;
+      
       default:
-        alert("caracter invalido")
         break;
+      }
+      productos = Number(prompt("1 - "+prod1.tipo+" "+prod1.nombre+" $"+prod1.precio+"\n 2 - "+prod2.tipo+" "+prod2.nombre+" $"+prod2.precio+"\n 3 - "+prod3.tipo+" "+prod3.nombre+" $"+prod3.precio+"\n 4 - "+prod1.tipo+" "+prod4.nombre+" $"+prod4.precio+ "\n -1 - Eliminar producto \n 0 - Salir"))
+    }   
+    let p="";
+    carrito.forEach(producto=> {
+    p= p + "\n" + indice + "- " + producto.tipo + " " + producto.nombre + " $" + producto.precio 
+    indice++;
+    })
+    total= calcularTotal(carrito)
+
+    alert( p + " \n El total de la compra es de: " + total)
+    if (total != 0) {
+      tipoPago(total)
     }
-    productos = Number(prompt("1-mother $5500 2-procesador $7000 3-memoria $5000 4-fuente $4500 "))
   }
-  alert("el total de la compra es de: " + total)
-  if (total != 0) {
-    tipoPago(total)
-  }
-}
 
 const tipoPago = (total) => {
   let pago = prompt("como quiere abonar sus productos, tarjeta o efectivo")
   if (pago == "tarjeta") {
     total *= 1.1
-    alert("el total es: " + total)
+    alert("El total es: " + total)
   } else if (pago == "efectivo") {
     total -= 1500
-    alert("pagos en efectivo superiores a $4000 llevan $1500 de descuento, el total es:" + total)
+    alert("Pagos en efectivo superiores a $4000 llevan $1500 de descuento, el total es: " + total)
   }
   else {
-    alert("caracter invalido")
+    alert("Caracter invalido")
   }
   alert("Muchas gracias, Saludos")
 }
@@ -53,5 +89,16 @@ const tipoPago = (total) => {
 const cantidad = (cant, precio) => {
   return cant * precio
 }
+
+const calcularTotal = (carrito) => {
+  let total = 0;
+  console.log(carrito)
+  carrito.forEach(producto=> {
+    console.log(producto.precio)
+    total+=producto.precio
+  });
+  return total;
+}
+
 
 compra()
